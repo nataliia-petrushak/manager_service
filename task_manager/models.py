a5779682535af192eddc192c9086ccb1038481ea
Amirammi <amirammina@gmail.com>
2023-04-28 11:50:53 +0300

    Add Worker & Task models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,15 +1,60 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class TaskType(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
 
 
 class Position(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
 
     def __str__(self) -> str:
         return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        related_name="workers"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.username} (position: {self.position.name})"
+
+
+class Task(models.Model):
+
+    PRIORITY_CHOICES = [
+        ("U", "Urgent"),
+        ("H", "High"),
+        ("A", "Alarming"),
+        ("L", "Low")
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default="L"
+    )
+    task_type = models.ForeignKey(
+        TaskType,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} (priority: {self.priority}, deadline: {self.deadline})"
