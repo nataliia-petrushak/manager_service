298de3a15417f7965909e63f61e069f200bb4235
Amirammi <amirammina@gmail.com>
2023-04-28 12:33:06 +0300

    Create view for home page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Task, TaskType, Position
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "num_tasks": Task.objects.count(),
+        "num_task_types": TaskType.objects.count(),
+        "num_positions": Position.objects.count(),
+        "num_workers": get_user_model().objects.count()
+    }
+    return render(request, "task_manager/index.html", context=context)
