d9536e6709fe111a0109bd3646ea2a8bc650f807
Amirammi <amirammina@gmail.com>
2023-04-28 11:15:39 +0300

    Create TaskType and Position models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
