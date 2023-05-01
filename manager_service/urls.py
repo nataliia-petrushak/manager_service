4635a12b9ed5e1d21d5d7fd7b72f63400041bf31
Amirammi <amirammina@gmail.com>
2023-05-01 09:42:34 +0300

    Fixed error with urls

--- manager_service/urls.py
+++ manager_service/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="taxi_service"))
+    path("", include("task_manager.urls", namespace="task_manager")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from .views import index
 
-urlpatterns = {
+urlpatterns = [
     path("", index, name="index")
-}
+]
 
 app_name = "task_manager"
