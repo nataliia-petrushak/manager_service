5371e044e42eaa1d2b05529ca58636dd9ce6347f
Amirammi <amirammina@gmail.com>
2023-05-02 08:20:27 +0300

    Add new url addres to the worker list page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, WorkerListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list")
 ]
 
 app_name = "task_manager"
