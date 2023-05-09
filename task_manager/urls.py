8d1f97b73ad8f3dec2678da6f7739043e82a75e0
Amirammi <amirammina@gmail.com>
2023-05-09 12:41:26 +0300

    Add url to the task-type-list page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,16 +2,20 @@ from django.urls import path
 
 from .views import (
     index,
+    TaskTypeListView,
     WorkerListView,
     TaskListView,
+    TaskDetailView,
     WorkerDetailView
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("tasks/", TaskListView.as_view(), name="task-list")
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail")
 ]
 
 app_name = "task_manager"
