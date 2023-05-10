5a82ab28d3de62ac6ce0c6a55e46734098bfef9c
Amirammi <amirammina@gmail.com>
2023-05-10 14:11:41 +0300

    Installed crispy-bootstrap4

--- manager_service/settings.py
+++ manager_service/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "task_manager"
+    "task_manager",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -67,6 +69,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 WSGI_APPLICATION = "manager_service.wsgi.application"
 
