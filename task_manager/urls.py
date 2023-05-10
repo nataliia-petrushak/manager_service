5762c902ba4be388da3b99973279eaae857a3711
Amirammi <amirammina@gmail.com>
2023-05-10 13:23:07 +0300

    Create path to the position list page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     index,
     TaskTypeListView,
+    PositionListView,
     WorkerListView,
     TaskListView,
     TaskDetailView,
@@ -12,6 +13,7 @@ from .views import (
 urlpatterns = [
     path("", index, name="index"),
     path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
