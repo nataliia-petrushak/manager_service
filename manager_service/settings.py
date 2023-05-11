647a9e8eeddc2670fde817db2b252642b5ca710b
Amirammi <amirammina@gmail.com>
2023-05-11 14:43:06 +0300

    Add accounts to project urls, login-redir to settings and add login page

--- manager_service/settings.py
+++ manager_service/settings.py
@@ -108,6 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
+LOGIN_REDIRECT_URL = "tasks/"
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
