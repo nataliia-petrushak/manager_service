024efbb0c97e9006954599f7bf689f641ecc15e7
Amirammi <amirammina@gmail.com>
2023-05-11 19:55:47 +0300

    Add link at login for for registr & fixed LOGIN_REDIRECT

--- manager_service/settings.py
+++ manager_service/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "task_manager.Worker"
 
-LOGIN_REDIRECT_URL = "tasks/"
+LOGIN_REDIRECT_URL = "/tasks/"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
