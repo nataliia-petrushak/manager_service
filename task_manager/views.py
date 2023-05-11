0a1dd403a339ba0d17a5a7f8f9573864780fd26d
Amirammi <amirammina@gmail.com>
2023-05-11 12:41:02 +0300

    Add pagination in views to position & type-list and at the page

--- task_manager/views.py
+++ task_manager/views.py
@@ -30,6 +30,7 @@ class TaskTypeListView(generic.ListView):
     model = TaskType
     template_name = "task_manager/task_type_list.html"
     context_object_name = "task_type_list"
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(TaskTypeListView, self).get_context_data(**kwargs)
@@ -70,6 +71,7 @@ class TaskTypeDelete(generic.DeleteView):
 
 class PositionListView(generic.ListView):
     model = Position
+    paginate_by = 3
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PositionListView, self).get_context_data(**kwargs)
