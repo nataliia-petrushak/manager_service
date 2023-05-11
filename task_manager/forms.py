cacf0eaddb5a0a41be8a17b60a7c5ecd620096db
Amirammi <amirammina@gmail.com>
2023-05-11 20:03:25 +0300

    Add WorkerCreateForm in forms

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,11 +1,78 @@
+from crispy_forms.helper import FormHelper
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
-from .models import Task, Project, TaskType, Position
+from .models import Task, Project, TaskType, Position, Worker, Team
 
 
 class DateInput(forms.DateInput):
     input_type = "date"
+
+
+class WorkerCreateForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Username*",
+            "style": "padding: 10px"
+        }),
+        label="",
+        required=True
+    )
+    first_name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={
+            "placeholder": "First Name*",
+            "style": "padding: 10px"
+        }),
+        label="",
+        required=True
+    )
+    last_name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={
+            "placeholder": "Last Name*",
+            "style": "padding: 10px"
+        }),
+        label="",
+        required=True
+    )
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+            "placeholder": "Email*",
+            "style": "padding: 10px"
+        }),
+        required=True,
+        label=""
+    )
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "Password*",
+            "style": "padding: 10px"
+        }),
+        label="",
+        required=True
+    )
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            "placeholder": "Confirm password*",
+            "style": "padding: 10px"
+        }),
+        label="",
+        required=True
+    )
+    position = forms.ModelChoiceField(
+        queryset=Position.objects.all(),
+        required=False
+    )
+    team = forms.ModelChoiceField(
+        queryset=Team.objects.all(),
+        required=False
+    )
+
+    class Meta(UserCreationForm.Meta):
+        model = Worker
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "position", "team")
 
 
 class TaskForm(forms.ModelForm):
@@ -15,9 +82,8 @@ class TaskForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea(attrs={
         "style": "padding: 10px; height: 100px"
     }))
-    project = forms.ModelMultipleChoiceField(
-        queryset=Project.objects.all(),
-        widget=forms.RadioSelect
+    project = forms.ModelChoiceField(
+        queryset=Project.objects.all()
     )
     deadline = forms.DateField(widget=DateInput(attrs={
         "style": "padding: 5px 0 0 0; width: 200px"
@@ -26,9 +92,8 @@ class TaskForm(forms.ModelForm):
         widget=forms.RadioSelect,
         choices=Task.PRIORITY_CHOICES
     )
-    task_type = forms.ModelMultipleChoiceField(
+    task_type = forms.ModelChoiceField(
         queryset=TaskType.objects.all(),
-        widget=forms.RadioSelect
     )
     assignees = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
