29c1f166fc526d351a67f170b56b842b491f5763
Amirammi <amirammina@gmail.com>
2023-05-22 14:33:11 +0300

    Add task-manager-rpg4.onrender.com to ALLOWED_HOSTS

--- manager_service/settings.py
+++ manager_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "task-manager-rpg4.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
